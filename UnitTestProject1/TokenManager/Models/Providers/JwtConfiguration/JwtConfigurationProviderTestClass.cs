e99aa71417988bf349a04b1034e9c22bb4f59ffa
Igor Trafalski <igorek2691@gmail.com>
2018-10-14 13:13:14 +0200

    added UnitTests project

---
+++ UnitTestProject1/TokenManager/Models/Providers/JwtConfiguration/JwtConfigurationProviderTestClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.TokenManager.Models.Providers.JwtConfiguration
+{
+    [TestClass]
+    public class JwtConfigurationProviderTestClass
+    {
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+
+        }
+
+        [TestMethod]
+        public void MapPersonDtoToPersonTestMethod()
+        {
+
+        }
+    } 
+}
